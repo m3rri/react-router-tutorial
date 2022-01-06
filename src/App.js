94e947df9ff5206ecb2d3d4e450e60c3b2dd8a5c
김혜리 (KIM HYE RI) 대리 (인실리코AI 1팀) <hrkim2@seegene.com>
2022-01-06 14:47:01 +0900

    connect the URL & Add some links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom";
+
 export default function App() {
   return (
     <div>
       <h1>App initiation.</h1>
+      <nav
+        style={{borderBottom: "solid 1px", paddingBottom: '1rem'}}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
     </div>
   );
 }
+/*
+ * Link 컴포넌트는 react-router-dom이 나중에 <a> 태그로 렌더링 해준다. 클릭하여 다른 페이지에 접근할 수 있도록 하는 역할임
+ * Link = a / to = href
+ * to에 들어가는 경로는 해당 컴포넌트의 parentURL + 경로로 적용됨
+ * ".." 사용가능!
+ */
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import {render} from 'react-dom';
+import { BrowserRouter } from "react-router-dom"; //history API를 사용하여 URL와 UI를 동기화하는 라우터
 import App from './App';
 
 const rootElement = document.getElementById("root");
-render(<App/>, rootElement);
+render(
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>,
+    rootElement
+);

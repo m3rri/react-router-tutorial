e9d93493ae497be5c27a018eb08afd8e4798e8f0
김혜리 (KIM HYE RI) 대리 (인실리코AI 1팀) <hrkim2@seegene.com>
2022-01-07 13:16:50 +0900

    add some routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import {render} from 'react-dom';
-import { BrowserRouter } from "react-router-dom"; //history API를 사용하여 URL와 UI를 동기화하는 라우터
+import { BrowserRouter, Routes, Route } from "react-router-dom"; //history API를 사용하여 URL와 UI를 동기화하는 라우터
 import App from './App';
+import Expenses from './routes/expenses';
+import Invoices from './routes/invoices';
 
 const rootElement = document.getElementById("root");
 render(
     <BrowserRouter>
-        <App/>
+        <Routes>
+            <Route path="/" element={<App/>}/>
+            <Route path="/expenses" element={<Expenses/>}/>
+            <Route path="/invoices" element={<Invoices/>}/>
+        </Routes>
     </BrowserRouter>,
     rootElement
 );

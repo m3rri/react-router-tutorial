f549ab1710e9da515f076ad6e2d7f8ea65c98763
김혜리 (KIM HYE RI) 대리 (인실리코AI 1팀) <hrkim2@seegene.com>
2022-01-11 15:01:03 +0900

    Active Links & Search Params

--- src/routes/invoices.js
+++ src/routes/invoices.js
@@ -1,17 +1,44 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useSearchParams } from "react-router-dom";
 import { getInvoices } from "../data";
 
 export default function Invoices() {
     let invoices = getInvoices();
-    let links = invoices.map(invoice=>{
-      return <Link
-                style={{display: 'block', margin: '1rem 0'}}
+    let [searchParams, setSearchParams] = useSearchParams();
+    
+    let links = invoices.filter(invoice=>{
+      let filter = searchParams.get("filter");
+      if(!filter)
+        return true;
+      let name = invoice.name.toLowerCase();
+
+      return name.indexOf(filter.toLowerCase())>0;
+    }).map(invoice=>{
+      return <NavLink
+                style={({isActive})=>{
+                  return {
+                    display: "block",
+                    margin: "1rem 0",
+                    color: isActive ? "red" : "black"
+                  }
+                }}
                 to={`./${invoice.number}`}
                 key={invoice.number}
              >
                 {invoice.name}
-             </Link>
+             </NavLink>
     },[]);
+    
+    let inputs = <input
+      value = {searchParams.get("filter") || ""}
+      onChange = {e=>{
+        let filter = e.target.value;
+        if(filter){
+          setSearchParams({filter});
+        }else{
+          setSearchParams({});
+        }
+      }}
+    />;
 
     return (
       <div style={{ display: "flex" }}>
@@ -21,9 +48,13 @@ export default function Invoices() {
             padding: "1rem"
           }}
         >
+          {inputs}
           {links}
         </nav>
       <Outlet/>
       </div>
     );
   }
+/**
+ * Link -> NavLink
+ */
